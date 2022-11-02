7c59f989357737746ba0def31bba5f7724c70a07
Thib <thibault.cheneviere@telecomnancy.net>
2022-11-02 18:14:38 +0100

    [+] add comment on display functions

--- includes/display.h
+++ includes/display.h
@@ -32,16 +32,29 @@
 // @param isColor if the color is enable
 void path_display(path_list *l, int isColor);
 
-// Display the map of path with color
-// @param pl path list
+// Display a path in color or not
+// @param path the path to display
+// @param isColor if the color is enable
 void f_printp(const char *path, int isColor);
 
 // Display the help menu
 // @param isColor if the color is enable
 void display_help(int isColored);
 
+// Implementation of sprintf function
+// @param format the format of the string
+// @param args the arguments
+// @return the formatted string
 char *f_sprintf(const char *format, va_list args);
+
+// Implementation of printf function with color and style
+// @param format the format of the string
+// @param ... the arguments
 void f_printf(const char *format, ...);
+
+// Display a string or a color or a style
+// @param beg the beginning of the string
+// @param end the end of the string
 void f_cprintf(char **beg, char **end);
 
 #endif // FTC_DISPLAY_H
